312d68c6516109589331a893d272475ebe6191fc
jdogcodey <jonnydawson@hotmail.com>
2024-12-06 16:49:05 +0000

    Add correct content type - following tutorial#

--- demo_http.js
+++ demo_http.js
@@ -3,6 +3,7 @@ var http = require("http");
 //create a server object:
 http
   .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.write("Hello World!"); //write a response to the client
     res.end(); //end the response
   })
